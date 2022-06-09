f82c662c6888336c777e398c62ee4b4c8109a861
Lucas <lucascortooms@hotmail.com>
2022-06-09 15:14:35 +0200

    Update zeek2nmap.py

--- zeek2nmap.py
+++ zeek2nmap.py
@@ -34,8 +34,22 @@ if __name__ == '__main__':
         try:
             nm.scan(line, '21 - 443')
             nm.command_line()
+            
+             for host in nm.all_hosts():
+     print('----------------------------------------------------')
+     print('Host : %s (%s)' % (host, nm[host].hostname()))
+     print('State : %s' % nm[host].state())
+     for proto in nm[host].all_protocols():
+         print('----------')
+         print('Protocol : %s' % proto)
+ 
+         lport = nm[host][proto].keys()
+         lport.sort()
+         for port in lport:
+             print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
         except:
             print("Host not reachable")
+            
 
 
 
