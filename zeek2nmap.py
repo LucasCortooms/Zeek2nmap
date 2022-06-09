a5278bd2fc6760ed39a37033413d73079685095b
Lucas <lucascortooms@hotmail.com>
2022-06-09 15:55:55 +0200

    Update zeek2nmap.py

--- zeek2nmap.py
+++ zeek2nmap.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     #cut away quotes after IP ands store host IP's in array
     ip = []
     for line in cut:
-        if line not in ip:
-        ip2 = line.split('"')
-        ip.append(ip2[0])
+            if line not in ip:
+            ip2 = line.split('"')
+            ip.append(ip2[0])
         
     #nmap
     nm = nmap.PortScanner()
