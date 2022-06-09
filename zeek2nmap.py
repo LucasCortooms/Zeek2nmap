4fe335ea98c8afb4d6fe94a4e794053e305fd9cd
Lucas <lucascortooms@hotmail.com>
2022-06-09 15:56:51 +0200

    Update zeek2nmap.py

--- zeek2nmap.py
+++ zeek2nmap.py
@@ -3,34 +3,34 @@ import elasticsearch
 import nmap
 
 if __name__ == '__main__':
-    #Open log file and convert it to a new file in JSON format
-    with open('out.json',"w") as outfile:
+    # Open log file and convert it to a new file in JSON format
+    with open('out.json', "w") as outfile:
         for log_record in ParseZeekLogs("bruh.log[1992]", output_format="json", safe_headers=False):
             if log_record is not None:
                 outfile.write(log_record + "\n")
 
-    #Put JSON file lines into array
+    # Put JSON file lines into array
     res = []
 
     with open('out.json', 'r') as fp:
         for line in fp:
             res.append(line.strip())
 
-    #cut away timestamp from line
+    # cut away timestamp from line
     cut = []
     for line in res:
-        x = slice(37,100)
+        x = slice(37, 100)
         y = line[x]
         cut.append(y)
 
-    #cut away quotes after IP ands store host IP's in array
+    # cut away quotes after IP ands store host IP's in array
     ip = []
     for line in cut:
-            if line not in ip:
+        if line not in ip:
             ip2 = line.split('"')
             ip.append(ip2[0])
-        
-    #nmap
+
+    # nmap
     nm = nmap.PortScanner()
     for line in ip:
         try:
@@ -60,7 +60,6 @@ if __name__ == '__main__':
                 print(f'{line} port {i} is {res}.')
         except:
             print(f'{line} is not reachable')
-
 
 
 
