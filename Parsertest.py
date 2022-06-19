e6ab85d0689802483eeb3b5963885a067cb6484b
LucasCortooms <lucascortooms@hotmail.com>
2022-06-19 19:25:31 +0200

    Create Parsertest.py

---
+++ Parsertest.py
@@ -0,0 +1,17 @@
+from parsezeeklogs import ParseZeekLogs
+from elasticsearch import Elasticsearch
+import nmap
+import json
+import ndjson
+from io import StringIO
+import requests
+
+if __name__ == '__main__':
+    path = '/usr/local/zeek/logs/current/conn.log'
+    # Open log file and convert it to a new file in JSON format
+    with open('out.json', "w") as outfile:
+        for log_record in ParseZeekLogs(path, output_format="json", safe_headers=False):
+            if log_record is not None:
+                outfile.write(log_record + "\n")
+
+
