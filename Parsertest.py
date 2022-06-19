28891f701f717ac763677459967e6dc62b7e7a61
LucasCortooms <lucascortooms@hotmail.com>
2022-06-19 19:34:54 +0200

    Update Parsertest.py

--- Parsertest.py
+++ Parsertest.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     path = '/usr/local/zeek/logs/current/conn.log'
     # Open log file and convert it to a new file in JSON format
     with open('out.json', "w") as outfile:
-        for log_record in ParseZeekLogs(path, output_format="json", safe_headers=False, fields=["ts", "id.orig_h", "id.resp_h"]):
+        for log_record in ParseZeekLogs(path, output_format="json", safe_headers=False, fields=["id.orig_h"]):
             if log_record is not None:
                 outfile.write(log_record + "\n")
 
